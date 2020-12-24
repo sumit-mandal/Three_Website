a61559d77a589af1bde25d43f6dc67dc5058cf51
sumit <mandal.sumi00@gmail.com>
2020-12-24 22:25:48 +0530

    22:25 / 24-12-20

--- resume/blog/models.py
+++ resume/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=50)
@@ -8,3 +8,8 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:blog_detail', kwargs={
+                'blog_id':self.id
+        })
